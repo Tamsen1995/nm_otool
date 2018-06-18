b5f5ac23ced435c5e581c0f585d43d9408ba7371
Tamanh BUI <tbui@e2r5p3.42.fr>
2018-06-18 18:13:20 +0200

    [UPD] added the FAT_MAGIC case. up until now only the reversed case was taken care of

--- src/nm/main_nm.c
+++ src/nm/main_nm.c
@@ -9,23 +9,29 @@
 
 int main(int ac, char **av)
 {
+	int i;
 	int fd;
 	char *ptr;
 	struct stat buf;
 
-	if (ac != 2)
-		fatal("Please give me an argument !\n");
-	if ((fd = open(av[1], O_RDONLY)) < 0)
-		fatal("open");
-	if (fd != -1 && (fstat(fd, &buf) == -1))
-		fatal("fstat in the main");
-	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
-		fatal("mmap");
-	ft_nm(ptr); // This is where enter the main flow of the nm
-	if (munmap(ptr, buf.st_size) < 0)
+	i = 1;
+	while (i < ac)
 	{
-		perror("munmap");
-		return (EXIT_FAILURE);
+		if (ac < 2)
+			fatal("Please give me an argument !\n");
+		if ((fd = open(av[i], O_RDONLY)) < 0)
+			fatal("open");
+		if (fd != -1 && (fstat(fd, &buf) == -1))
+			fatal("fstat in the main");
+		if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+			fatal("mmap");
+		ft_nm(ptr); // This is where enter the main flow of the nm
+		if (munmap(ptr, buf.st_size) < 0)
+		{
+			perror("munmap");
+			return (EXIT_FAILURE);
+		}
+		i++;
 	}
 	return (EXIT_SUCCESS);
 }
