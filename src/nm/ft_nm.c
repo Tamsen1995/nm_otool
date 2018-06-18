b59f3072bed5f69ee35b593fe33762e6399d923e
Tamanh BUI <tbui@e2r5p3.42.fr>
2018-06-18 18:39:24 +0200

    [ADD] Whenever the nm has several arguments, filenames need to be displayed before the actual output

--- src/nm/ft_nm.c
+++ src/nm/ft_nm.c
@@ -7,12 +7,17 @@ void ft_nm(char *ptr)
 	magic_number = *(int *)ptr;
 	if (magic_number == MH_MAGIC)
 		handle_32(ptr);
+	else if (magic_number == MH_CIGAM)
+		fatal("Logic to swap bytes(BIG ENDIAN)");
 	else if (magic_number == MH_MAGIC_64)
 		handle_64(ptr);
 	else if (magic_number == MH_CIGAM_64)
-		ft_printf("Logic to swap bytes. (Little endian)"); // TODO : Implement
+		ft_printf("Logic to swap bytes. (Big endian)"); // TODO : Implement
 	else if (magic_number == FAT_CIGAM)
 		handle_fat_big(ptr);
 	else if (magic_number == FAT_MAGIC)
 		handle_fat_lil(ptr);
+	else
+		ft_printf("The file was not recognized as a valid object file\n");
+	
 }
