a956d972bce6027be85d6a04a4fde1b5fb6d27d2
Tamanh BUI <tbui@e2r13p6.42.fr>
2018-06-20 14:08:34 +0200

    [NORM]

--- src/nm/case_64/handle_64.c
+++ src/nm/case_64/handle_64.c
@@ -1,26 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_64.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/20 14:04:32 by tbui              #+#    #+#             */
+/*   Updated: 2018/06/20 14:04:33 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../includes/nm.h"
 
-void handle_64(char *ptr)
+void			handle_64(char *ptr)
 {
-	int ncmds;
-	int i;
-	struct mach_header_64 *header;
-	struct load_command *lc;
-	struct symtab_command *sym;
-	t_lsection *sec_list;
+	uint32_t				i;
+	struct mach_header_64	*header;
+	struct load_command		*lc;
+	struct symtab_command	*sym;
+	t_lsection				*sec_list;
 
 	header = (struct mach_header_64 *)ptr;
-	ncmds = header->ncmds;
 	i = 0;
 	lc = (void *)ptr + sizeof(*header);
 	sec_list = get_sections_64(ptr);
-	while (i < ncmds)
+	while (i < header->ncmds)
 	{
 		if (lc->cmd == LC_SYMTAB)
 		{
 			sym = (struct symtab_command *)lc;
 			process_symtab_64(sym, ptr, sec_list);
-			break;
+			break ;
 		}
 		i++;
 		lc = (void *)lc + lc->cmdsize;
